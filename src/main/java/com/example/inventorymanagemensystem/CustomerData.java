e6aec4f890936da9d9fd5c74f9d78cefe32a30ac
visteen <visteen192@gmail.com>
2023-07-01 17:30:21 +0000

    customer class added

---
+++ src/main/java/com/example/inventorymanagemensystem/CustomerData.java
@@ -0,0 +1,46 @@
+package com.example.inventorymanagemensystem;
+
+import java.sql.Date;
+
+public class CustomerData {
+    private Integer customerID;
+    private Integer quantity;
+    private double price;
+    private String goodName;
+    private  String goodCategory;
+    private Date date;
+
+
+    public CustomerData(Integer customerID,Integer quantity,double price,String goodName,String goodCategory,Date date){
+        this.customerID = customerID;
+        this.date = date;
+        this.price = price;
+        this.goodCategory = goodCategory;
+        this.goodName = goodName;
+        this.quantity = quantity;
+    }
+
+    public Integer getCustomerID() {
+        return customerID;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getGoodName() {
+        return goodName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getGoodCategory() {
+        return goodCategory;
+    }
+}
